02ef6c3e5cbb6409f81387eea7536ebffdb5f15b
Praveen <praveenthanikachalam408@gmail.com>
2025-09-15 01:42:40 +0530

    for vercel

--- app/components/Editor.tsx
+++ app/components/Editor.tsx
@@ -101,7 +101,9 @@ export const Editor = () => {
       });
 
       const connect = (script: string, attempt = 1) => {
-        const socket = new WebSocket("ws://theoremotion.onrender.com/ws/run-stream");
+        const socket = new WebSocket(
+          "wss://theoremotion.onrender.com/ws/run-stream",
+        );
 
         socket.onopen = () => {
           console.log("Connected to backend");
